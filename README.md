2faf1082d745216da0fdff85663a6ddfe8064938
nieweglp <nieweglp@gmail.com>
2025-02-28 02:31:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+
+Pub Sub Topic Name: backend-events-topic
+Pub Sub Topic Subscription: backend-events-topic-sub
+Note: All events pushed to one single topic
+
+
+### Order Events Schema
+```JSON
+{
+    "event_type": "order",
+    "order_id": "uuid",
+    "customer_id": "uuid",
+    "order_date": "timestamp",
+    "status": "enum(pending, processing, shipped, delivered)",
+    "items": [
+        {
+            "product_id": "uuid",
+            "product_name": "string",
+            "quantity": "integer",
+            "price": "float"
+        }
+    ],
+    "shipping_address": {
+        "street": "string",
+        "city": "string",
+        "country": "string"
+    },
+    "total_amount": "float"
+}
+```
+
+### Inventory Events Schema
+```JSON
+{
+    "event_type": "inventory",
+    "inventory_id": "uuid",
+    "product_id": "uuid",
+    "warehouse_id": "uuid",
+    "quantity_change": "integer(-100 to 100)",
+    "reason": "enum(restock, sale, return, damage)",
+    "timestamp": "timestamp"
+}
+```
+
+### User Activity Events Schema
+```JSON
+{
+    "event_type": "user_activity",
+    "user_id": "uuid",
+    "activity_type": "enum(login, logout, view_product, add_to_cart, remove_from_cart)",
+    "ip_address": "string",
+    "user_agent": "string",
+    "timestamp": "timestamp",
+    "metadata": {
+        "session_id": "uuid",
+        "platform": "enum(web, mobile, tablet)"
+    }
+}
+```
+
+### Data Modeling and Architecture
+#### Design the BigQuery data model that will store these events. 
+
+#### Table structure and relationships
+Tables:
+- orders
+- items
+- shipping_address
+- inventory
+- user_activity
+
+
+#### Partitioning and clustering strategies
+Clustering works good on frequently filtered columns. That's why I choose e.g. customer_id in orders or user_id in user_activity.
+
+Primary keys are unique and clustering by them is not an effective. Clustering has a sense, when values are repeating.
+
+
+#### Tracking historical data and time travel.
+BigQuery natively supports time travel and change tracking via snapshots. It allows users to query historical data from up to 7 days prior by specifying a timestamp in the FOR SYSTEM_TIME AS OF clause, enabling recovery and analysis of data as it existed at a specific point in time.
+
+#### Deliverables:
+Data model diagram in gymshark-schema.png
+
+DDL statements for creating tables in ddl directory.
+
+Explanation of design decisions.
+
---
+++ ddl/db_init.sql
@@ -0,0 +1,83 @@
+CREATE SCHEMA IF NOT EXISTS `gymshark.data_us`
+OPTIONS(location="US");
+
+-- Orders Table
+CREATE TABLE `gymshark.data_us.orders` (
+    order_id STRING,  -- UUID, PK
+    customer_id STRING,  -- UUID
+    order_date DATE,
+    status STRING,  -- ENUM(pending, processing, shipped, delivered)
+    total_amount FLOAT64,
+    load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    source_file STRING,
+    batch_id STRING
+)
+PARTITION BY DATE(order_date)
+CLUSTER BY customer_id;
+
+-- Items Table 
+CREATE TABLE `gymshark.data_us.items` (
+    items_id STRING, -- UUID, PK
+    product_id STRING,  -- UUID
+    order_id STRING,  -- UUID, orders FK
+    product_name STRING,
+    quantity INT64,
+    price FLOAT64,
+    load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    source_file STRING,
+    batch_id STRING
+)
+CLUSTER BY order_id;
+
+-- Shipping Address Table
+CREATE TABLE `gymshark.data_us.shipping_address` (
+    shipping_address_id STRING, -- UUID, PK
+    order_id STRING,  -- UUID, orders FK
+    street STRING,
+    city STRING,
+    country STRING,
+    load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    source_file STRING,
+    batch_id STRING
+)
+CLUSTER BY order_id;
+
+-- Inventory Table
+CREATE TABLE `gymshark.data_us.inventory` (
+    inventory_id STRING,  -- UUID, PK
+    product_id STRING,  -- UUID, items FK
+    warehouse_id STRING,  -- UUID
+    quantity_change INT64,
+    reason STRING,  -- ENUM(restock, sale, return, damage)
+    timestamp TIMESTAMP,
+    load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    source_file STRING,
+    batch_id STRING
+)
+PARTITION BY DATE(timestamp)
+CLUSTER BY product_id, warehouse_id;
+
+-- User Activity Table
+CREATE TABLE `gymshark.data_us.user_activity` (
+    user_activity_id STRING, -- UUID, PK
+    user_id STRING,  -- UUID
+    session_id STRING,  -- UUID
+    activity_type STRING,  -- ENUM(login, logout, view_product, add_to_cart, remove_from_cart)
+    ip_address STRING,
+    user_agent STRING,
+    platform STRING,  -- ENUM(web, mobile, tablet)
+    timestamp TIMESTAMP,
+    load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    source_file STRING,
+    batch_id STRING
+)
+PARTITION BY DATE(timestamp)
+CLUSTER BY user_id, session_id;
+
+-- User Customer bridge table
+CREATE TABLE `gymshark.data_us.user_customer_bridge` (
+    user_customer_bridge_id STRING, -- UUID, PK
+    user_id STRING, -- UUID
+    customer_id STRING, -- UUID
+)
+CLUSTER BY user_id, customer_id;
